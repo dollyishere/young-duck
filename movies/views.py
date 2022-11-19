a8d16aac28f8cdacb74b08fdf920d7449b731a50
dollyishere <rundollyrun8@gmail.com>
2022-11-20 04:05:02 +0900

    feat[BE]: movie app 기초 기능 구현(detail, recommended)

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import Genre, People, Movie
+
 
 # Create your views here.
+def detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    context = {
+        'movie' : movie,
+    }
+    return render(request, 'movies/detail.html', context)
+
+
+def recommended(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies' : movies,
+    }
+    return render(request, 'movies/recommended.html', context)
