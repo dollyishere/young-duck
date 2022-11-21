c12c7c4e16a6ffa718812e114cb99aa047c5fdd2
dollyishere <rundollyrun8@gmail.com>
2022-11-22 02:57:21 +0900

    feat[BE]: 신규 field 추가해서 credit_list.json 재생성(db 차후 수정)

---
+++ TMDB_API/tmdb_api_credit_list.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+TMDB_API_KEY = '851d868671f3d56e21c372701537e231'
+
+def get_credit_datas():
+    credit_list = []
+    for i in credit_dict:
+        # 'https://www.themoviedb.org/person/{}'.format(person.pk) 사용하면 인물 소개로 이동 가능
+        request_url = 'https://api.themoviedb.org/3/person/{}?api_key={}&language=ko-KR'.format(i, TMDB_API_KEY)
+        person = requests.get(request_url).json()
+
+        if person['profile_path'] == None:
+            person['profile_path'] = ''
+
+        if person['birthday'] == None:
+            person['birthday'] = ''
+
+        data = {
+            'model' : 'movies.people',
+            'pk' : person['id'],
+            'fields' : {
+                'name' : person['name'],
+                'birthday' : person['birthday'],
+                'known_for_department' : person['known_for_department'],
+                'popularity' : person['popularity'],
+                'biography' : person['biography'],
+                'profile_path' : person['profile_path'],
+                }
+            }
+        credit_list.append(data)
+
+    with open("credit_list.json", "w", encoding="utf-8") as w:
+        json.dump(credit_list, w, indent="\t", ensure_ascii=False)
+
+
+credit_json = open('credit.json', encoding='utf-8')
+credit_dict = json.load(credit_json)
+
+
+get_credit_datas()
